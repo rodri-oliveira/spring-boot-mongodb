d7397983800d0d9d8816f86dd78f1882d7900d0b
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2024-05-21 09:32:58 -0300

    Inserção de usuário com POST

--- src/main/java/com/rodrigoolive/workshopmongo/services/UserService.java
+++ src/main/java/com/rodrigoolive/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigoolive.workshopmongo.domain.User;
+import com.rodrigoolive.workshopmongo.dto.UserDTO;
 import com.rodrigoolive.workshopmongo.repository.UserRepository;
 import com.rodrigoolive.workshopmongo.services.exception.ObjectNotFoundExcption;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundExcption("Object not found with id: " + id));
+    }
+    
+    public User insert(User obj) {
+    	return repo.insert(obj);
+    }
+    
+    public User fromDTO(UserDTO objDTO) {
+    	return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
